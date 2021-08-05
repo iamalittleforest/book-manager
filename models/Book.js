92dedbfe35593b3a48dc251c223cc8c751969f24
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-05 13:18:23 -0700

    define Book model

---
+++ models/Book.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model { }
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+    modelName: 'book'
+  }
+);
+
+module.exports = Book;
